4f5fb1d5e5edb2a9d09008a185e244ad3263bd04
Severin Schoepke <severin.schoepke@gmail.com>
2015-03-20 10:54:32 +0100

    Configuring gcc explicitly to use C99 standard

--- script/configure_build_dev.sh
+++ script/configure_build_dev.sh
@@ -6,12 +6,12 @@ printf "Configuring nginx build for development...\n"
 
 pushd "vendor"
 pushd "nginx-1.4.7"
-CFLAGS="-g -O0 -Wall" ./configure     \
-    --with-debug                      \
-    --prefix=$(pwd)/../../build/nginx \
-    --conf-path=conf/nginx.conf       \
-    --error-log-path=logs/error.log   \
-    --http-log-path=logs/access.log   \
+CFLAGS="-g -O0 -Wall -std=c99" ./configure \
+    --with-debug                           \
+    --prefix=$(pwd)/../../build/nginx      \
+    --conf-path=conf/nginx.conf            \
+    --error-log-path=logs/error.log        \
+    --http-log-path=logs/access.log        \
     --add-module=../../ngx_http_scm_query_server_proxy_module
 popd
 popd
